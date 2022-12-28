33fcb5569951312495a8ea2290121d2c35a57387
Smeagol2069 <Thesmeagol360@gmail.com>
2022-12-27 21:56:53 -0500

    Adds esc key to normal mode

--- nvim/lua/smeagol/remap.lua
+++ nvim/lua/smeagol/remap.lua
@@ -3,3 +3,5 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Typing w in command mode writes all
 vim.keymap.set("c", "w", vim.cmd.wall)
+-- Normal mode esc ends highlights and searches
+vim.keymap.set("n", "<Esc>", ":nohl<CR>:echo<CR>")

d338f1a68c99fb6cb0cb08c55df7d89dc10dd843
Smeagol2069 <Thesmeagol360@gmail.com>
2023-04-12 11:13:06 -0400

    Desktop

--- nvim/lua/smeagol/plugins/lsp-config/main.lua
+++ nvim/lua/smeagol/plugins/lsp-config/main.lua
@@ -1,5 +1,7 @@
 require('mason').setup()
-require('mason-lspconfig').setup()
+require('mason-lspconfig').setup {
+		ensure_installed = { 'lua_ls' },
+}
 
 require'lspconfig'.lua_ls.setup {
   settings = {

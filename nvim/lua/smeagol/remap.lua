0abc683b95e615fc2483c7709693e32bc1e2db4e
Smeagol2069 <Thesmeagol360@gmail.com>
2023-04-13 13:39:42 -0400

    Desktop

--- nvim/lua/smeagol/remap.lua
+++ nvim/lua/smeagol/remap.lua
@@ -7,3 +7,11 @@ vim.keymap.set("n", "<Esc>", ":nohl<CR>:echo<CR>")
 -- Makes J and K visually line wise rather than absolute
 vim.keymap.set("n", "j", "gj")
 vim.keymap.set("n", "k", "gk")
+
+-- Accessing the lsp documentation
+vim.keymap("i", "<C-f>", function()
+    vim.cmd.stopinsert()
+    vim.lsp.buf.signature_help()
+    vim.defer_fn(function() vim.cmd.wincmd("w") end, 100)
+    vim.keymap.set("n", "q", ":close<CR>", { buffer = true })
+end)

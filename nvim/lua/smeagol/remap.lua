0b69332d3da5bc1e860c0ee440c4aca4b0320e82
Smeagol2069 <Thesmeagol360@gmail.com>
2023-04-13 13:51:29 -0400

    Desktop

--- nvim/lua/smeagol/remap.lua
+++ nvim/lua/smeagol/remap.lua
@@ -1,17 +1,20 @@
+local keymap = vim.keymap.set
 -- Leader then pv opens the explorer menu
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+keymap("n", "<leader>pv", vim.cmd.Ex)
 -- Typing control + s in normal mode writes all
-vim.keymap.set("n", "<C-s>", vim.cmd.wall)
+keymap("n", "<C-s>", vim.cmd.wall)
 -- Normal mode esc ends highlights and searches
-vim.keymap.set("n", "<Esc>", ":nohl<CR>:echo<CR>")
+keymap("n", "<Esc>", ":nohl<CR>:echo<CR>")
 -- Makes J and K visually line wise rather than absolute
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+keymap("n", "j", "gj")
+keymap("n", "k", "gk")
 
 -- Accessing the lsp documentation
-vim.keymap("i", "<C-f>", function()
+keymap("i", "<C-f>", function()
     vim.cmd.stopinsert()
     vim.lsp.buf.signature_help()
     vim.defer_fn(function() vim.cmd.wincmd("w") end, 100)
-    vim.keymap.set("n", "q", ":close<CR>", { buffer = true })
-end)
+    keymap("n", "q", ":close<CR>", { buffer = true })
+end
+)
+

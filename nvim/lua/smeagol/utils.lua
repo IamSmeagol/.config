3289f2f616c08842dacd30a6c97792f760f049e9
Smeagol2069 <Thesmeagol360@gmail.com>
2023-04-16 19:20:32 -0400

    Adds vertical help, started reorganizing requires

--- nvim/lua/smeagol/utils.lua
+++ nvim/lua/smeagol/utils.lua
@@ -1,5 +1,3 @@
-require("smeagol/remap")
-
 -- Allows + and * registers to go to Windows system keyboard
 vim.g.clipboard = {
                    name = 'WslClipboard',
@@ -14,11 +12,25 @@ vim.g.clipboard = {
                    cache_enabled = 0,
                  }
 
+-- Open help window in a vertical split to the right.
+vim.api.nvim_create_autocmd("BufWinEnter", {
+    group = vim.api.nvim_create_augroup("help_window_right", {}),
+    pattern = { "*.txt" },
+    callback = function()
+        if vim.o.filetype == 'help' then vim.cmd.wincmd("L") end
+    end
+})
 
-vim.o.relativenumber = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 3
--- set autoindent
 
+vim.opt.relativenumber = true
 
+vim.opt.tabstop = 4
+
+vim.opt.shiftwidth = 4
+
+vim.opt.splitright = true
+
+vim.opt.fillchars = 'vert:|'
+
+-- Sets theme
 vim.cmd[[colorscheme nord]]

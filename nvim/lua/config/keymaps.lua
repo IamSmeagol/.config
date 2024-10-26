642d3f985500d9efb886f625a54ae614b896ea4c
Smeagol2069 <Thesmeagol360@gmail.com>
2024-10-26 12:18:12 -0400

    i removed nvim from wsl config, it's now a symbolic link

--- nvim/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local keymap = vim.keymap.set
-
--- Makes J and K visually line wise rather than absolute
-keymap("n", "j", "gj")
-keymap("n", "k", "gk")

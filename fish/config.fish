1017e6cf1df1816cd5658d996f51966db472e563
Smeagol <Thesmeagol360@gmail.com>
2024-11-23 09:31:30 -0500

    adds vi mode to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+# Vi mode that lets me accept suggestion with control + f like default fish
+set -g fish_key_bindings fish_vi_key_bindings
+bind -M insert \cf accept-autosuggestion
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end

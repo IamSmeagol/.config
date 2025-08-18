ff10e9550550c5eefa157419dda6b949589edfc5
IamSmeagol <Thesmeagol360@gmail.com>
2025-08-18 18:37:41 -0400

    qol -- help files show in bufferline

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  callback = function(ev)
+    if vim.bo[ev.buf].buftype == "help" then
+      vim.bo[ev.buf].buflisted = true
+    end
+  end,
+})
